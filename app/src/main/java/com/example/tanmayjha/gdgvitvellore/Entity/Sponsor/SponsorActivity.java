ee2775d7bbb2b4bfdc2317b48340aa07bb79304f
AKANSHI SRIVASTAVA <AKANSHI SRIVASTAVA>
2016-10-15 18:18:07 +0530

    Sponsor

--- app/src/main/java/com/example/tanmayjha/gdgvitvellore/Entity/Sponsor/SponsorActivity.java
+++ app/src/main/java/com/example/tanmayjha/gdgvitvellore/Entity/Sponsor/SponsorActivity.java
@@ -10,6 +10,6 @@ public class SponsorActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.sponsors_main);
+        setContentView(R.layout.sponsor);
     }
 }
