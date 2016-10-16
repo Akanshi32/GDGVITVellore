1f22989390d5fb663c2053998187922f36a47eeb
AKANSHI SRIVASTAVA <AKANSHI SRIVASTAVA>
2016-10-16 18:14:16 +0530

    SplashScreen and UI

---
+++ app/src/main/java/com/example/tanmayjha/gdgvitvellore/Entity/SplashScreen/SplashScreenActivity.java
@@ -0,0 +1,33 @@
+package com.example.tanmayjha.gdgvitvellore.Entity.SplashScreen;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.tanmayjha.gdgvitvellore.Entity.LogIn.LoginActivity;
+import com.example.tanmayjha.gdgvitvellore.R;
+
+public class SplashScreenActivity extends Activity {
+
+    private final int SPLASH_DISPLAY_DURATION = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+
+                Intent mainIntent = new Intent(SplashScreenActivity.this,LoginActivity.class);
+                SplashScreenActivity.this.startActivity(mainIntent);
+                SplashScreenActivity.this.finish();
+            }
+        }, SPLASH_DISPLAY_DURATION);
+    }
+}
