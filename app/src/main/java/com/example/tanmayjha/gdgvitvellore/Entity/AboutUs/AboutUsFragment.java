abfc81810fddc8774e35db6205c15c77269a6d2e
tanmay777 <tanmay.jha1@gmail.com>
2016-12-17 00:41:02 +0530

    About us updated

--- app/src/main/java/com/example/tanmayjha/gdgvitvellore/Entity/AboutUs/AboutUsFragment.java
+++ app/src/main/java/com/example/tanmayjha/gdgvitvellore/Entity/AboutUs/AboutUsFragment.java
@@ -3,10 +3,12 @@ package com.example.tanmayjha.gdgvitvellore.Entity.AboutUs;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.TextInputLayout;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
@@ -21,6 +23,7 @@ public class AboutUsFragment extends Fragment {
     Button button;
     Spinner spinnerOption;
     EditText feedback;
+    TextInputLayout inputFeedback;
 
     public AboutUsFragment() {
         // Required empty public constructor
@@ -35,17 +38,35 @@ public class AboutUsFragment extends Fragment {
         button=(Button)view.findViewById(R.id.submit);
         spinnerOption=(Spinner)view.findViewById(R.id.spinner_option);
         feedback=(EditText)view.findViewById(R.id.feedback);
+        inputFeedback=(TextInputLayout)view.findViewById(R.id.input_feedback);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view){
-                String subject=String.valueOf(spinnerOption.getSelectedItem());
-                Intent email = new Intent(Intent.ACTION_SEND);
-                String message=feedback.getText().toString();
-                email.putExtra(Intent.EXTRA_EMAIL,new String[]{"tanmay.jha1@gmail.com"});
-                email.putExtra(Intent.EXTRA_SUBJECT, subject);
-                email.putExtra(Intent.EXTRA_TEXT, message);
-                email.setType("message/rfc822");
-                startActivity(Intent.createChooser(email, "Choose an Email client :"));
+
+                if(feedback.getText().toString().trim().isEmpty())
+                {
+                    inputFeedback.setError("Enter the text first");
+                    if(feedback.requestFocus())
+                    {
+                        //not sure what is the use of below code
+                        getActivity().getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE);
+                    }
+                    inputFeedback.setErrorEnabled(true);
+
+                }
+                else
+                {
+                    //below statement is used to remove the error statement i guess.
+                    inputFeedback.setErrorEnabled(false);
+                    String subject=String.valueOf(spinnerOption.getSelectedItem());
+                    Intent email = new Intent(Intent.ACTION_SEND);
+                    String message=feedback.getText().toString();
+                    email.putExtra(Intent.EXTRA_EMAIL,new String[]{"tanmay.jha1@gmail.com"});
+                    email.putExtra(Intent.EXTRA_SUBJECT, subject);
+                    email.putExtra(Intent.EXTRA_TEXT, message);
+                    email.setType("message/rfc822");
+                    startActivity(Intent.createChooser(email, "Choose an Email client :"));
+                }
             }
         });
     }
